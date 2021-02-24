a8981f7feaeda63b70ae738bb05246a7a14d4bc6
Joe Karlsson <joekarlsson1@gmail.com>
2021-02-24 12:14:37 -0600

    posting new missions works.

--- pages/api/mission/[id].ts
+++ pages/api/mission/[id].ts
@@ -14,8 +14,20 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 	switch (method) {
 		case 'GET':
 			// Get data from your database
-			const query = { _id }
-			const mission = await missions.findOne(query)
+			let query = { _id }
+			let mission = await missions.findOne(query)
+
+			if (!mission) {
+				// There was no matching mission
+				return res.status(404).json({ message: `Mission not found` })
+			}
+
+			res.status(200).json({ mission })
+			break
+		case 'DELETE':
+			// Delete mission from your database
+			query = { _id }
+			mission = await missions.deleteOne(query)
 
 			if (!mission) {
 				// There was no matching mission
@@ -25,7 +37,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 			res.status(200).json({ mission })
 			break
 		default:
-			res.setHeader('Allow', ['GET'])
+			res.setHeader('Allow', ['GET', 'DELETE'])
 			res.status(405).end(`Method ${method} Not Allowed`)
 	}
 }
